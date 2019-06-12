366986654e8f2943472572fc4e140dc2c0a47a6e
marvin evins <maevins@cps.edu>
2019-06-12 10:28:33 -0500

    mongodb for node api

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import config from './config';
+import routes from './routes';
+
+
+let app = express();
+app.server = http.createServer(app);
+
+//middleware
+
+
+
+//passport config
+
+
+//api routes v1
+app.use('/v1', routes);
+
+app.server.lisen(config.port);
+console.log(`started on port ${app.server.address().port}`);
+
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+import express fro 'express';
+import config from '../config';
+import middleware from '../middleware';
